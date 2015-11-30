2777a8e83b6ae54a31d2a5c6946697dec8ad468c
aniket <aoak12@ufl.edu>
2015-11-29 22:01:36 -0800

    Added a function to calculate the norm of a vector

---
+++ tst/aoak/projects/hobby/dsp/transforms/utils/VectorHelperTest.java
@@ -0,0 +1,20 @@
+package aoak.projects.hobby.dsp.transforms.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import aoak.projects.hobby.dsp.transforms.utils.VectorHelper;;
+
+public class VectorHelperTest {
+
+	@Test
+	public void normTest() {
+		// db3 scaling filter as a vector
+		List<Double> vec = Arrays.asList(0.2352, 0.5706, 0.3252, -0.0955, -0.0604, 0.0249);
+		assertEquals(0.7071, VectorHelper.getNorm(vec), 1E-2);
+	}
+}
